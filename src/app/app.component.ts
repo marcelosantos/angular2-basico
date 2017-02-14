0b7cc70e78ab3e4fb2e86a60765337b15fb363c4
Marcelo Santos <marcelosantosadm@gmail.com>
2017-02-14 02:43:26 +0000

    adicionando comportamento two way binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,18 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'my-app',
-  template: `<h1>Angular 2 {{name}}</h1>`,
+  selector: 'meu-teste-basico',
+  template: `
+    <h1>Angular 2 {{name}}</h1>
+    <input type="text" [(ngModel)]="name"/>
+    <input type="text" [(ngModel)]="name"/>
+    <button (click)="sayHello()">Clique Me</button>
+  `,
 })
-export class AppComponent  { name = 'Básico'; }
+export class AppComponent  {
+    name = 'Básico';
+
+    sayHello(){
+        console.log("clicado!")
+    }
+}
